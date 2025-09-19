aecb4530f84ed2d34567f81977a28407f06b03eb
Andreas Bichinger <andreas.bichinger@gmail.com>
2025-09-19 14:10:58 +0200

    Add Google Drive filetypes to new file modal

--- src/components/modals/ModalNewFile.vue
+++ src/components/modals/ModalNewFile.vue
@@ -6,7 +6,24 @@
         <div class="vuefinder__new-file-modal__form">
           <p class="vuefinder__new-file-modal__description">{{ t('Create a new file') }}</p>
           <input v-model="name" @keyup.enter="createFile"
-                 class="vuefinder__new-file-modal__input" :placeholder="t('File Name')" type="text">
+                 class="vuefinder__new-file-modal__input" :placeholder="t('File Name')" type="text" />
+          
+          <div v-if="filetypes.length > 1" class="vuefinder__new-file-modal__filetype ">
+            <div class="vuefinder__new-file-modal__filetype-input">
+              <label for="mimetype" class="vuefinder__new-file-modal__label">
+                {{ t('Filetype') }}
+              </label>
+            </div>
+            <div class="vuefinder__new-file-modal__filetype-label">
+              <select id="mimetype" v-model="mimetype"
+                      class="vuefinder__new-file-modal__select">
+                <optgroup :label="t('Filetype')">
+                  <option v-for="{name, mimetype} in filetypes" :value="mimetype">{{ name }}</option>
+                </optgroup>
+              </select>
+            </div>
+          </div>
+
           <message v-if="message.length" @hidden="message=''" error>{{ message }}</message>
         </div>
       </div>
@@ -32,6 +49,41 @@ const {t} = app.i18n;
 const name = ref('');
 const message = ref('');
 
+/**
+ * @returns {string | undefined}
+ */
+function getFilesystem() {
+  const storageInfo = app.fs.data.storage_info ?? {}
+  return storageInfo[app.fs.adapter]?.filesystem
+}
+
+const filetypes = [
+  {
+    name: t("Plain text"),
+    mimetype: ''
+  },
+]
+
+const isGoogleDriveFS = (getFilesystem() ?? "").includes("GoogleDriveFS")
+if (isGoogleDriveFS) {
+  filetypes.push(
+    {
+      name: t("Google Doc"),
+      mimetype: "application/vnd.google-apps.document"
+    },
+    {
+      name: t("Google Sheet"),
+      mimetype: "application/vnd.google-apps.spreadsheet"
+    },
+    {
+      name: t("Google Slide"),
+      mimetype: "application/vnd.google-apps.presentation"
+    },
+  )
+}
+
+const mimetype = ref(filetypes[0]?.mimetype ?? '')
+
 const createFile = () => {
   if (name.value !== '') {
     app.emitter.emit('vf-fetch', {
@@ -40,6 +92,7 @@ const createFile = () => {
         m: 'post',
         adapter: app.fs.adapter,
         path: app.fs.data.dirname,
+        ...(mimetype.value ? {mimetype: mimetype.value} : {}),
       },
       body: {
         name: name.value
